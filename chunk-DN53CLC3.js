42b73325f91edd64cae8bea323035c2c1798c030
VenomViper2 <97921320+VenomViper2@users.noreply.github.com>
2024-12-07 16:51:12 +0000

    Deploying to gh-pages from @ VenomViper2/webber-web@d26d14ed3f5cb7a6b193c739f39ede9018032f09 🚀

---
+++ chunk-DN53CLC3.js
@@ -0,0 +1,1 @@
+import{Ca as c,Da as p,R as n,va as o,wa as a}from"./chunk-VJCTQKUQ.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-contact"]],standalone:!0,features:[p],decls:2,vars:0,template:function(t,m){t&1&&(o(0,"p"),c(1,"contact works!"),a())}})};export{s as ContactComponent};
