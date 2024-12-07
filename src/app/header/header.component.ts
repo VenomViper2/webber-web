4dc5c78083655e5bd2c68b2caf33482e197c33e9
Tom <tomptaylor92@googlemail.com>
2024-12-07 18:05:25 +0000

    Add hamburger for header when screen is small

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,25 @@
+// header.component.ts
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [
-    RouterLinkActive,
-    RouterLink
-  ],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    document.body.style.overflow = this.isMenuOpen ? 'hidden' : '';
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+    document.body.style.overflow = '';
+  }
 }
