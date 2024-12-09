e99dbc4bdd72837a6628593474f3998b3af70b5c
VenomViper2 <97921320+VenomViper2@users.noreply.github.com>
2024-12-09 22:14:21 +0000

    Deploying to gh-pages from @ VenomViper2/webber-web@96ed0a4d578b8f9670732932da33dd4f965d47b5 🚀

--- chunk-WP3GJOR5.js
+++
@@ -1,1 +0,0 @@
-import{Ga as n,Ha as m,Pa as a,Ra as p,U as o}from"./chunk-BK57SHRL.js";var s=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-home"]],standalone:!0,features:[p],decls:2,vars:0,template:function(e,r){e&1&&(n(0,"p"),a(1,"home works!"),m())}})};export{s as HomeComponent};
