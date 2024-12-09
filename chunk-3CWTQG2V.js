57cc8cf7d5b6bb15f785379e35bc4bcf6aceb1c8
VenomViper2 <97921320+VenomViper2@users.noreply.github.com>
2024-12-09 21:25:03 +0000

    Deploying to gh-pages from @ VenomViper2/webber-web@0568e3f64eea7f9bcfdb861f58b2bbd015378254 🚀

---
+++ chunk-3CWTQG2V.js
@@ -0,0 +1,1 @@
+import{Ea as n,Fa as m,Na as a,Pa as p,S as o}from"./chunk-NBJIYC4C.js";var s=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-home"]],standalone:!0,features:[p],decls:2,vars:0,template:function(e,r){e&1&&(n(0,"p"),a(1,"home works!"),m())}})};export{s as HomeComponent};
--- chunk-SH3P7KEL.js
+++
@@ -1,1 +0,0 @@
-import{Fa as c,Ga as p,S as n,ya as o,za as a}from"./chunk-EUGMUAQS.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-contact"]],standalone:!0,features:[p],decls:2,vars:0,template:function(t,m){t&1&&(o(0,"p"),c(1,"contact works!"),a())}})};export{s as ContactComponent};
