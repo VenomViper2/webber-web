80445eb5680d6e51ea0acc9a620736512a718fa5
Tom <tomptaylor92@googlemail.com>
2024-12-09 21:54:29 +0000

    Implement basic application service

---
+++ src/app/model/App.ts
@@ -0,0 +1,14 @@
+export interface App {
+  id: string;
+  name: string;
+  description: string;
+  fullDescription: string;
+  features: string[];
+  images: string[];
+}
+export interface AppCard {
+  id: string;
+  name: string;
+  description: string;
+  imagePath: string;
+}
---
+++ src/app/service/application.service.ts
@@ -0,0 +1,45 @@
+import {Injectable} from '@angular/core';
+import {App, AppCard} from '../model/App';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApplicationService {
+  private applications: App[] = [];
+
+  constructor(private http: HttpClient) {
+    this.loadApps();
+  }
+
+  getCards(): AppCard[] {
+    return this.applications.map((app: App) => ({
+      id: app.id,
+      name: app.name,
+      description: app.description,
+      imagePath: app.images[1]
+    }));
+  }
+
+
+  getAllApps(): App[] {
+    return this.applications;
+  }
+
+  getApp(id: string): App | undefined {
+    return this.applications.find(app => app.id === id);
+  }
+
+  private loadApps() {
+    this.http.get<{ apps: App[] }>('/assets/apps.json')
+      .subscribe({
+        next: (data) => {
+          this.applications = data.apps;
+        },
+        error: (error) => {
+          console.error('Error loading apps:', error);
+        }
+      });
+  }
+
+}
