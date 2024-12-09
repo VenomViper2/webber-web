a9edb5e3665fe664afc8fc3660668317dc7932d5
Tom <tomptaylor92@googlemail.com>
2024-12-09 23:01:14 +0000

    Automatic reformat code

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,4 +1,3 @@
-// header.component.ts
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { CommonModule } from '@angular/common';
