07729f49e3ad1691b4c99577d8cc316349b170e7
VenomViper2 <97921320+VenomViper2@users.noreply.github.com>
2024-12-09 22:30:00 +0000

    Deploying to gh-pages from @ VenomViper2/webber-web@7ed8be79bb267f07f0ca950dedd0f783e72b4b2b 🚀

---
+++ chunk-ZXQWIVEH.js
@@ -0,0 +1,1 @@
+import{Ha as o,Ia as a,Qa as c,Sa as p,U as n}from"./chunk-DCZ6ONVW.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=n({type:e,selectors:[["app-contact"]],standalone:!0,features:[p],decls:2,vars:0,template:function(t,m){t&1&&(o(0,"p"),c(1,"contact works!"),a())}})};export{s as ContactComponent};
