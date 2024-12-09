589292927e9f539b9686369d974f869bede4e5e8
VenomViper2 <97921320+VenomViper2@users.noreply.github.com>
2024-12-09 23:18:07 +0000

    Deploying to gh-pages from @ VenomViper2/webber-web@29b6f3941dab96555110d791b8f830f2392d1c30 🚀

--- chunk-Q5PRM3B4.js
+++
@@ -1,1 +0,0 @@
-import{Ha as n,Ia as m,Qa as a,Sa as p,U as o}from"./chunk-DCZ6ONVW.js";var s=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-home"]],standalone:!0,features:[p],decls:2,vars:0,template:function(e,r){e&1&&(n(0,"p"),a(1,"home works!"),m())}})};export{s as HomeComponent};
--- chunk-UVKLQHZX.js
+++
@@ -1,1 +0,0 @@
-import{Ha as n,Ia as a,Qa as p,Sa as s,U as o}from"./chunk-DCZ6ONVW.js";var i=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=o({type:t,selectors:[["app-about"]],standalone:!0,features:[s],decls:2,vars:0,template:function(e,r){e&1&&(n(0,"p"),p(1,"about works!"),a())}})};export{i as AboutComponent};
