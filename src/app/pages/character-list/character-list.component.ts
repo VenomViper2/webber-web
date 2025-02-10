adb4798cbcef6fdd53d42ce7167829720143f333
Tom <tomptaylor92@googlemail.com>
2025-02-10 20:40:12 +0000

    Fix json error and globalise some styles

--- src/app/pages/character-list/character-list.component.ts
+++ src/app/pages/character-list/character-list.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { Character } from "../../model/Character";
+import { CharacterService } from "../../service/character.service";
 
 @Component({
-  selector: 'app-character-list',
+  selector: 'character-list',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './character-list.component.html',
   styleUrl: './character-list.component.css'
 })
-export class CharacterListComponent {
+export class CharacterListComponent implements OnInit {
+  characters: Character[] = [];
 
+  constructor(private characterService: CharacterService) {
+
+  }
+
+  ngOnInit(): void {
+    this.characterService.getAllCharacters().subscribe(character => {
+      this.characters = character;
+    });
+  }
 }
