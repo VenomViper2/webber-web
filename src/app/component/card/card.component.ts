485b90d36b2e94ff2cd6e8a8adab807466c80e52
Tom <tomptaylor92@googlemail.com>
2025-02-11 00:12:33 +0000

    Update Card component style

--- src/app/component/card/card.component.ts
+++ src/app/component/card/card.component.ts
@@ -1,15 +1,22 @@
 import { Component, Input } from '@angular/core';
+import { NgClass } from "@angular/common";
+import { RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-card',
     standalone: true,
-    imports: [],
+    imports: [
+        NgClass,
+        RouterLink
+    ],
     templateUrl: './card.component.html',
     styleUrl: './card.component.css'
 })
 export class CardComponent {
     @Input() name?: string;
     @Input() badge?: string;
+    @Input() link?: string;
     @Input() description?: string;
+    @Input() type: 'blue' | 'red' | 'green' | 'yellow' | 'gold' = 'gold';
 
 }
--- src/app/pages/feat-list/feat-list.component.ts
+++ src/app/pages/feat-list/feat-list.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { FeatService } from "../../service/feat.service";
 import { Feat } from "../../model/Feat";
+import { CardComponent } from "../../component/card/card.component";
 
 @Component({
   selector: 'feat-list',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    CardComponent
   ],
   templateUrl: './feat-list.component.html',
   styleUrl: './feat-list.component.css'
@@ -24,5 +26,21 @@ export class FeatListComponent implements OnInit {
     this.featService.getAllFeats().subscribe(feats => {
       this.feats = feats;
     });
+  }
+
+  getColor(featName: string): "blue" | "red" | "green" | "yellow" | "gold" {
+    if (featName.toLowerCase().includes('red')) {
+      return 'red';
+    }
+    if (featName.toLowerCase().includes('blue')) {
+      return 'blue';
+    }
+    if (featName.toLowerCase().includes('green')) {
+      return 'green';
+    }
+    if (featName.toLowerCase().includes('gold')) {
+      return 'gold';
+    }
+    return 'gold';
   }
 }
